f222f15eb5ca3b2d95b6ba0da5bc72115e515b3f
dhkang.bsn <dhkang.bsn@gmail.com>
2021-08-13 15:00:30 +0900

    '1'

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreationView
+from articleapp.views import ArticleCreationView, ArticleDetailView
 
 app_name='articleapp'
 
 urlpatterns = [
-    path('create/', ArticleCreationView.as_view(), name="create")
+    path('create/', ArticleCreationView.as_view(), name="create"),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name="detail"),
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -10,5 +11,11 @@ from articleapp.models import Article
 class ArticleCreationView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = 'accountapp/hello_world.html'
+    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'articleapp/create.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
+    form_class = ArticleCreationForm
